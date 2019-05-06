6e5620c6e73bf15fe5f017e91910f6d499454c8f
jamieberrier <berrier.jamie@gmail.com>
2019-05-06 17:51:18 +0000

    created config/environment.rb

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+#require_relative '../lib/balboa_park_itinerary_ideas/cli'
+#require_relative '../lib/balboa_park_itinerary_ideas/scraper'
+#require_relative '../lib/balboa_park_itinerary_ideas/itinerary'
+require_relative '../lib/balboa_park_itinerary_ideas/version'
