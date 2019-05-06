55dca48a29c82c9fb9530a82924d26316f7a5876
jamieberrier <berrier.jamie@gmail.com>
2019-05-06 22:25:10 +0000

    updated scraper.rb

--- lib/balboa_park_itinerary_ideas/scraper.rb
+++ lib/balboa_park_itinerary_ideas/scraper.rb
@@ -5,9 +5,23 @@ class BalboaParkItineraryIdeas::Scraper
   end
 
   def self.print_list
-    self.get_page.css('div.view-itineraries span.field-content').each.with_index(1) do |it, i|
-      name = it.css('a').text
-      puts " #{i}. #{name}"
+    self.get_page.css('div.view-itineraries span.field-content').each.with_index(1) do |event, i|
+      event_name = event.css('a').text
+      puts " #{i}. #{event_name}"
     end
+  end
+
+  def self.scrape_itineraries
+    itineraries = []
+
+    self.get_page.css('div.view-itineraries span.field-content').each do |itinerary|
+      itineraries.push(name: itinerary.css('a').text, url: itinerary.css('a').attribute("href").value)
+    end
+    itineraries
+  end
+
+  # scrape an itinerary's page to get further information about that itinerary.
+  def self.scrape_event_page(event_url)
+    event_page = Nokogiri::HTML(open(event_url))
   end
 end
