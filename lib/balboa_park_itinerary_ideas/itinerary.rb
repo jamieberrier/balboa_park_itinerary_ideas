dcfe38feef33205916c9ede1287ef1366e366e61
jamieberrier <berrier.jamie@gmail.com>
2019-05-14 18:25:53 +0000

    added comments & deleted old code

--- lib/balboa_park_itinerary_ideas/itinerary.rb
+++ lib/balboa_park_itinerary_ideas/itinerary.rb
@@ -9,12 +9,14 @@ class BalboaParkItineraryIdeas::Itinerary
     @@all << self
   end
 
+  # creates itinerary instances from an array of scraped itineraries
   def self.create_from_collection(itineraries_array)
     itineraries_array.each do |itinerary|
       BalboaParkItineraryIdeas::Itinerary.new(itinerary)
     end
   end
 
+  # add the itinerary's summary and details of its attractions from a hash of scraped attributes
   def add_itinerary_attributes(itinerary_hash)
     itinerary_hash.each {|key, value| self.send(("#{key}="), value)}
     self
@@ -24,10 +26,13 @@ class BalboaParkItineraryIdeas::Itinerary
     @@all
   end
 
+  # returns the itinerary that matches the user input
   def self.find(id)
     self.all[id-1]
   end
 
+  # prints the details of each attraction
+  # add the attraction's url, if it has one
   def print_attractions
     self.attractions.each do |x|
       puts "#{x[:name]}".bold
@@ -36,31 +41,3 @@ class BalboaParkItineraryIdeas::Itinerary
     end
   end
 end
-=begin
-  # Itinerary's page
-  def doc
-    # refactor...add 'https://www.balboapark.org' to itinerary url
-    @doc ||= Nokogiri::HTML(open(BalboaParkItineraryIdeas::Scraper::URL + self.itinerary_url))
-  end
-
-  def header
-    @header ||= doc.css('h1.page-header').text.strip
-  end
-
-  def summary
-    @summary ||= doc.css('div.content div.field--type-text-with-summary p').text.strip
-  end
-
-  # add URL for each attraction, if exists
-  def get_details
-    @details = []
-
-    doc.css('div.field--name-field-stops div.field--item').each do |attraction|
-        description = attraction.css('p').text
-        name = attraction.css('div.content').text.delete("\n").strip.split("Attraction").join.strip.split("Description").delete_at(0)
-
-        @details.push(name: name, description: description)
-    end
-    @details.reject! { |e|  e[:name] == nil}
-  end
-=end
